ce189c9b301231eb0c09e81f2aaaf1cf8fcb6614
Jali Rainio <jali.rainio@sofokus.com>
2020-12-22 12:00:07 +0200

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,37 @@
 import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
 
+const style = {
+  border: '1px solid grey',
+  padding: '3px 5px'
+}
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = React.useState(false)
+
+  return (<div style={style} className='blog-entry'>
+    <p>
+      {blog.title} {blog.author}
+      <button onClick={() => setDetailsVisible(!detailsVisible)}>
+        {detailsVisible ? 'Hide' : 'Show'}
+      </button>
+    </p>
+    {detailsVisible && (
+      <div>
+        <p>
+          {blog.url}
+        </p>
+        <p>
+          Likes: {blog.likes}
+          <button>Like</button>
+        </p>
+        {blog.user && blog.user.name && (
+          <p>
+            {blog.user.name}
+          </p>
+        )}
+      </div>
+    )}
+  </div>
+  )
+}
 export default Blog
